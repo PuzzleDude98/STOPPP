d278f8d97411078019efa4526045709e71b3fa3c
PuzzleDude98 <puzzledude98@gmail.com>
2025-07-09 13:46:51 -0700

    V 1.1.1
    critical error fix whoops

--- src/client/java/net/pd98/mixin/client/ConnectionScreenMixin.java
+++ src/client/java/net/pd98/mixin/client/ConnectionScreenMixin.java
@@ -29,10 +29,11 @@ public class ConnectionScreenMixin {
 		return config.blockedServers.stream().anyMatch(lowered::contains);
 	}
 
-	@Inject(method = "Lnet/minecraft/client/gui/screen/multiplayer/ConnectScreen;connect(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;ZLnet/minecraft/client/network/CookieStorage;)V", at = @At("HEAD"), cancellable = true)
+	@Inject(method = "connect(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/network/ServerAddress;Lnet/minecraft/client/network/ServerInfo;ZLnet/minecraft/client/network/CookieStorage;)V", at = @At("HEAD"), cancellable = true)
 	private static void onConnect(Screen screen, MinecraftClient client, ServerAddress address, ServerInfo info, boolean quickPlay, CookieStorage cookieStorage, CallbackInfo ci) {
 
 		if (isBlocked(address.getAddress())) {
+			ci.cancel();
 			client.setScreen(new DisconnectedScreen(
 					new MultiplayerScreen(new TitleScreen()),
 					Text.literal(config.errorTextLine1),
